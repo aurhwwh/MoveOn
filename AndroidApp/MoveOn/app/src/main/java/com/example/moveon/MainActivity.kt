dc4e6488c3f7433ee585870b1a480dc3e9e9d7f6
aurhwwh <balzievaur@gmail.com>
2026-02-24 23:03:51 +0300

    [Android App]: demo changes

--- AndroidApp/MoveOn/app/src/main/java/com/example/moveon/MainActivity.kt
+++ AndroidApp/MoveOn/app/src/main/java/com/example/moveon/MainActivity.kt
@@ -10,10 +10,13 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
@@ -28,12 +31,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import com.example.moveon.ui.theme.MoveOnTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,6 +48,11 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         setContent {
             LazyColumn() {
+                item {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(text = "Welcome to MoveOn!", color = Color.Black, fontFamily = FontFamily.SansSerif, fontSize = 30.sp,modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally))
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
                 itemsIndexed(listOf(
                     EventData(R.drawable.img,"Football","Let's play"),
                     EventData(R.drawable.img,"Football","Let's play"))) {
