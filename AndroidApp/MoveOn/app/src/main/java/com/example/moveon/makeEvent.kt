a758e3d63affb72676199bb6d239ce7beac55629
Diana <di.andronikk@gmail.com>
2026-02-24 22:07:32 +0300

    [Android App]: demo events page

---
+++ AndroidApp/MoveOn/app/src/main/java/com/example/moveon/makeEvent.kt
@@ -0,0 +1,44 @@
+package com.example.moveon
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun MakeEvent(data: EventData) {
+    Card(modifier = Modifier.fillMaxWidth().padding(5.dp).padding(top = 20.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+    ) {
+        Box() {
+            Row() {
+                Image(painter = painterResource(id = data.imageId),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.padding(5.dp).size(64.dp).clip(CircleShape))
+
+                Column(modifier = Modifier.padding(start = 10.dp).padding(top = 8.dp)) {
+                    Text(text = data.name)
+                    Text(text = data.description)
+                }
+            }
+
+        }
+    }
+}
