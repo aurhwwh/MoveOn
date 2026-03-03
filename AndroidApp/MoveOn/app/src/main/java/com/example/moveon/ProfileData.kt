120d483b0866faf4bdd84a4c1909c99ade2c3d9a
Diana <di.andronikk@gmail.com>
2026-03-03 20:34:02 +0300

    [Android App]: profile screen demo

---
+++ AndroidApp/MoveOn/app/src/main/java/com/example/moveon/ProfileData.kt
@@ -0,0 +1,10 @@
+package com.example.moveon
+
+class ProfileData (
+    val imageId: Int,
+    val name: String,
+    val birth: String,
+    val city: String,
+    val rating: Double,
+    val description: String
+)
