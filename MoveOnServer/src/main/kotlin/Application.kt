cda1937c84455c6e8720bf709481c080ee57551b
Vsevolod Timofeev <vssetimofeev@edu.hse.ru>
2026-03-16 18:42:22 +0300

    [Server]: fix database

--- MoveOnServer/src/main/kotlin/Application.kt
+++ MoveOnServer/src/main/kotlin/Application.kt
@@ -1,5 +1,6 @@
 package MoveOn
 
+import MoveOn.database.Database
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
@@ -7,6 +8,7 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    Database
     configureSerialization()
     configureSecurity()
     configureRouting()
